82f1904c1e09b6b1e8f56cd557cd4a3f38a3e5ac
Hardik Kanjariya <hkdevs@hardikkanajariya.in>
2026-06-22 10:45:20 +0530

    perf: optimize initial font loading and enable production build minification settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    legalComments: 'none',
+    minifyIdentifiers: true,
+    minifySyntax: true,
+    minifyWhitespace: true,
+  },
+  build: {
+    minify: 'esbuild',
+    cssMinify: true,
+    sourcemap: false,
+  }
 })
